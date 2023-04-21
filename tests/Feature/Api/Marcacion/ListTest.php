31d0e1b20e233a621abd7e77aff0ab35a4524cd5
Jose Ascurra <joseascurra123@gmail.com>
2023-04-21 15:41:28 -0400

    Agregado del test para obtener solo las marcaciones del usuario de la sesion actual

--- tests/Feature/Api/Marcacion/ListTest.php
+++ tests/Feature/Api/Marcacion/ListTest.php
@@ -32,4 +32,19 @@ class ListTest extends TestCase
         $response = $this->getJson(route("api.marcaciones.index"));
         $response->assertUnauthorized();
     }
+
+    /**
+     * @test
+     */
+    public function can_list_only_all_marcaciones_join_to_user(): void
+    {
+        $user = User::factory()->create();
+        $user2 = User::factory()->create();
+        Marcacion::factory()->for($user2)->count(10)->create();
+        Marcacion::factory()->for($user)->create();
+        Sanctum::actingAs($user,["*"]);
+        $response = $this->getJson(route("api.marcaciones.index"));
+        $response->assertSuccessful();
+        $response->assertJsonCount(1,"data");
+    }
 }
